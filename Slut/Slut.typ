410b3fde39bde69d68410a6a81f3ddffd652321a
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-05-28 11:14:49 +0200

    Set up slut presentation files

---
+++ Slut/Slut.typ
@@ -0,0 +1,14 @@
+#import "@preview/slydst:0.1.4": *
+
+
+#show: slides.with(
+  title: "Slydst: Slides with Typst",
+  subtitle: none,
+  date: none,
+  authors: ("Gaspard Lambrechts",),
+  layout: "medium",
+  ratio: 4 / 3,
+  title-color: none,
+)
+
+= Yo gooner
---
+++ Slut/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems
+        (system:
+          let pkgs = nixpkgsFor.${system}; in {
+            default = pkgs.mkShell
+              {
+                buildInputs = with pkgs; [
+                  typst
+                  typstfmt
+                  tinymist
+                ];
+              };
+          });
+    };
+}
